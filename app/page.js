974c59ac9429f2b48c1ef9767edd59f841da67a3
Arnav Joshi <arnvjshi@gmail.com>
2025-03-22 23:05:59 +0530

    vercel deployment boom

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import { MadeForYou } from "@/components/made-for-you"
 export default function Home() {
   return (
     <div className="space-y-8 pb-20">
+    <meta>Arnav{"You're not supposed to see this "}</meta>
       <HomeHero />
       <RecentlyPlayed />
       <ForYou />
